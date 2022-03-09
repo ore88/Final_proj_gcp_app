51019f63ee1d92fa73722cf742d8634efd14836a
ore88 <ore.ajulo@gmail.com>
2022-03-09 04:47:43 +0000

    fixed main file

--- main.py
+++ main.py
@@ -5,66 +5,13 @@ from flask import Flask,request, url_for, redirect, render_template, jsonify
 import pickle
 from google.cloud import storage
 
-from dotenv import load_dotenv
-
-load_dotenv() 
-
-config = {
-    "type": os.getenv("Type"),
-    "project_id": os.getenv("Project_id"),
-    "private_key_id": os.getenv("Private_key_id"),
-    "private_key": oos.getenv("Private_key"), 
-    "client_email": os.getenv("Client_email"),
-    "client_id": os.getenv("Client_id"),
-    "auth_uri": os.getenv("Auth_uri"),
-    "token_uri": os.getenv("Token_uri"),
-    "auth_provider_x509_cert_url": os.getenv("Auth_provider_x_cert_url"),
-    "client_x509_cert_url": os.getenv("Client_x_cert_url")
-  }
-
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'config'
-storage_client = storage.Client()
-
-CLOUD_PROJECT = 'grand-drive-342813' # GCP project name
-bucket_name = 'msdsproject_finlproj' # bucket name for storage of your model
-BUCKET = 'gs://' + CLOUD_PROJECT + '-{}'.format(bucket_name)
-
-def download_blob(project_name, bucket_name, source_blob_name, destination_file_name):
-    """Downloads a blob from the bucket."""
-    # bucket_name = "your-bucket-name"
-    # source_blob_name = "storage-object-name"
-    # destination_file_name = "local/path/to/file"
-
-    storage_client = storage.Client(project_name)
-
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(source_blob_name)
-    
-    if destination_file_name is not None: 
-      blob.download_to_filename(destination_file_name)
-      print(
-          "Blob {} downloaded to {}.".format(
-              source_blob_name, destination_file_name
-          )
-      )     
-    return blob
 
 app = Flask(__name__)
 
-outfile_name = model_dir + 'knn-reg101-gcp-downloaded'
-model_gcp_src = str(model_name)+'.pkl'
-model_downloaded = download_blob(CLOUD_PROJECT, bucket_name, model_gcp_src, 'knn_model.pkl')
-
 # Loading the model for predictions
-model= load_model(knn_model)
+model= load_model('tuned_knn_gcp')
 
-# Predictions from deployed model
-# new_prediction_gcp = predict_model(gcp_final_knn, data=df_test)
-
-cols = ['battery_power','blue','clock_speed','dual_sim',	
-        'fc','four_g','int_memory','m_dep','mobile_wt','n_cores','pc',
-        'px_width','ram','sc_h','sc_w','talk_time','three_g','touch_screen',
-        'wifi']
+cols = ['battery_power','blue','clock_speed','dual_sim','fc','four_g','int_memory','m_dep','mobile_wt','n_cores','pc','px_width','ram','sc_h','sc_w','talk_time','three_g','touch_screen','wifi']
 
 @app.route('/')
 def home():
